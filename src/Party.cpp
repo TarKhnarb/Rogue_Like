297891670229442fdb2ea469b47b1b368bafc84e
Yliandis <sami.grolleau@gmail.com>
2020-04-23 22:08:06 +0200

    compilation ok

--- src/Party.cpp
+++ src/Party.cpp
@@ -985,26 +985,29 @@ void Party::updateScrollingMenu(){
                     break;
 
                 case sf::Keyboard::Return:// on appui sur entrer on applique le choix selectionné
-                    std::string action = textScrolling[scrollingIndex].getString();
+					{
+						std::string action = textScrolling[scrollingIndex].getString();
 
-                    if(action == std::string("use")){// on utilise la potion donc on régénère la vie du player
+						if(action == std::string("use")){// on utilise la potion donc on régénère la vie du player
 
-                    }
-                    else if(action == std::string("throw")){ // on jette l'item par terre
+						}
+						else if(action == std::string("throw")){ // on jette l'item par terre
 
-                    }
-                    else if(action == std::string("move")){ // on déplace l'objet là ou le player choisi l'emplacement via le cursor de playerInventory
+						}
+						else if(action == std::string("move")){ // on déplace l'objet là ou le player choisi l'emplacement via le cursor de playerInventory
 
-                    }
-                    else if(action == std::string("equip")){ // On équip l'objet dans le stuff
+						}
+						else if(action == std::string("equip")){ // On équip l'objet dans le stuff
 
-                    }
-                    else if(action == std::string("unequip")) {
+						}
+						else if(action == std::string("unequip")) {
 
-                    }
-                    else if(action == std::string("add to inventory")){ // on ajoute l'objet du coffre a l'inventaire
+						}
+						else if(action == std::string("add to inventory")){ // on ajoute l'objet du coffre a l'inventaire
 
-                    }
+						}
+					}
+                    break;
 
                 default:
                     break;
